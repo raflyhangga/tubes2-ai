2035329f31d74a1a31e676f23b7f310ab2fd0266
bastianhs <140167233+bastianhs@users.noreply.github.com>
2024-12-14 16:10:35 +0700

    feat: add knn from scratch

--- src/scratch/knn.py
+++ src/scratch/knn.py
@@ -1,4 +1,55 @@
-class KNN:
-    def __init__(self):
-        print("Ini konstruktor")
-        pass
+import numpy as np
+
+
+class KNNScratch:
+    def __init__(self, n_neighbors: int = 3, metric: str = "euclidean", power: float = None):
+        self.__n_neighbors: int = n_neighbors
+        if metric == "minkowski":
+            self.__power: float = power
+        elif metric == "manhattan":
+            self.__power: float = 1.0
+        elif metric == "euclidean":
+            self.__power: float = 2.0
+        elif metric == "chebyshev":
+            self.__power: float = np.inf
+        else:
+            raise ValueError("Invalid distance type")
+
+    def fit(self, x: np.ndarray, y: np.ndarray):
+        self.__x: np.ndarray = x
+        self.__y: np.ndarray = y
+        return self
+    
+    def predict(self, x: np.ndarray):
+        y_pred = np.array([self.__predict_single(x_i) for x_i in x])
+        return y_pred
+    
+    def __predict_single(self, x: np.ndarray):
+        distances = np.array([self.__calculate_distance(x, x_i) for x_i in self.__x])
+        k_nearest_x_indices = np.argsort(distances)[:self.__n_neighbors]
+        k_nearest_y = np.array([self.__y[i] for i in k_nearest_x_indices])
+        most_common = np.bincount(k_nearest_y).argmax()
+        return most_common
+    
+    def __calculate_distance(self, x1: np.ndarray, x2: np.ndarray):
+        distance = np.linalg.norm(x=x1 - x2, ord=self.__power)
+        return distance
+
+
+# Examples
+if __name__ == "__main__":
+
+    from sklearn.datasets import load_iris
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+
+    iris = load_iris()
+    x, y = iris.data, iris.target
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    
+    knn_scratch = KNNScratch(n_neighbors=5, metric="minkowski", power=3)
+    knn_scratch.fit(x_train, y_train)
+    y_pred = knn_scratch.predict(x_test)
+    
+    acc = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {acc}")
