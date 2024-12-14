73a5432d78d24a0288a5fbb0d33d86ae2ad1e155
Agil0975 <agilfadillahsabri03@gmail.com>
2024-12-14 18:27:59 +0700

    feat: now can train model with ID3

--- src/scratch/id3.py
+++ src/scratch/id3.py
@@ -1,26 +1,34 @@
 import pandas as pd
 import numpy as np
 
-class Node:
-    def __init__(self, attribute: str, value: str|int|float|bool, children: dict):
-        self.attribute = attribute
-        self.value = value
-        self.children = children
+class TreeNode:
+    def __init__(self, node: str, branch: dict):
+        self.node = node        # attribute name
+        self.branch = branch    # dictionary of branch (key: branch value, value: TreeNode)
+
+    def print_tree(self, level: int = 0):
+        """function for printing the tree"""
+        for key, value in self.branch.items():
+            print("    " * level, end="")
+            print(f'{self.node} {key}')
+            if value is not None:
+                value.print_tree(level + 1)
 
 class ID3:
     def __init__(self):
-        self.tree : Node = None
-        self.list_attribute_names : list = ["Outlook", "Temperature", "Humidity", "Wind"] # None       HARDCODED
-        self.list_attribute_types : dict = {"Outlook": "categorical", "Temperature": "categorical", "Temperature2": "numerical", "Humidity": "categorical", "Wind": "categorical"} # None       HARDCODED
-        self.target_attribute : str = "PlayTennis" # None       HARDCODED
-        self.target_attribute_type : str = None
-        self.list_target_attribute_values : list = ["Yes", "No"] # None       HARDCODED
+        self.tree : TreeNode = None
+        self.list_attribute_names : list = None
+        self.list_attribute_types : dict = None
+        self.target_attribute : str = None
+        self.list_target_attribute_values : list = None
 
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
     
     """ 
         Function for training ID3 model 
     """
-    def train(self, data: dict, attribute: list[str], parent_data: dict) -> None:
+    def train(self, data: dict, attribute: list[str], parent_data: dict) -> TreeNode:
         """function for training ID3 model
 
         Args:
@@ -28,8 +36,29 @@ class ID3:
             attribute (list[str]): list of remaining attribute
             parent_data (dict): data from parent node
         """
-        print("Ini train")
-        pass
+        if len(data) == 0:
+            return TreeNode(self.target_attribute, {"= " + str(self.plurality_value(parent_data, self.target_attribute)): None})
+        elif self.is_homogeneous(data):
+            return TreeNode(self.target_attribute, {"= " + (data[self.target_attribute].unique()[0]): None})
+        elif len(attribute) == 0:
+            return TreeNode(self.target_attribute, {"= " + (self.plurality_value(data, self.target_attribute)): None})
+        else:
+            best_attribute, break_point = self.find_best_attribute(data, attribute)
+            tree = TreeNode(best_attribute, {})
+            if self.list_attribute_types[best_attribute] == "categorical":
+                for value in data[best_attribute].unique():
+                    new_data = data[data[best_attribute] == value]  # data with best_attribute = value
+                    new_attribute = attribute.copy()                # remaining attribute
+                    new_attribute.remove(best_attribute)
+                    tree.branch["= " + value] = self.train(new_data, new_attribute, data)
+            else:
+                new_data_less_than = data[data[best_attribute] < break_point]
+                new_data_greater_than = data[data[best_attribute] >= break_point]
+                new_attribute = attribute.copy()                    # remaining attribute
+                new_attribute.remove(best_attribute)
+                tree.branch["< " + str(break_point)] = self.train(new_data_less_than, new_attribute, data)
+                tree.branch[">= " + str(break_point)] = self.train(new_data_greater_than, new_attribute, data)
+            return tree
 
     def plurality_value(self, data: dict, attribute: str) -> str|int|float|bool|None:
         """function for finding the most common value in data
@@ -90,7 +119,7 @@ class ID3:
             entropy_children = len(data_less_than) / len(data) * self.entropy(data_less_than) + len(data_greater_than) / len(data) * self.entropy(data_greater_than)
         return entropy_parent - entropy_children
 
-    def find_best_attribute(self, data: dict, attribute: list[str]) -> str:
+    def find_best_attribute(self, data: dict, attribute: list[str]) -> tuple:
         """function for finding the best attribute
 
         Args:
@@ -98,9 +127,25 @@ class ID3:
             attribute (list[str]): list of attribute
 
         Returns:
-            str: the best attribute
+            tuple: best attribute and break point (if the best attribute is continuous)
         """
-        return max(attribute, key=lambda x: self.information_gain(data, x))
+        max_gain = 0
+        best_attribute = None
+        break_point = None
+        for attr in attribute:
+            if self.list_attribute_types[attr] == "categorical":
+                gain = self.information_gain(data, attr)
+                if gain > max_gain:
+                    max_gain = gain
+                    best_attribute = attr
+            else:
+                temp_break_point = self.break_point(data, attr)
+                gain = self.information_gain(data, attr, temp_break_point)
+                if gain > max_gain:
+                    max_gain = gain
+                    best_attribute = attr
+                    break_point = temp_break_point
+        return best_attribute, break_point
 
     def break_point(self, data: dict, attribute: str) -> dict:
         """function for finding break point of continuous data
@@ -114,42 +159,17 @@ class ID3:
         """
         max_gain = 0
         best_break_point = None
-        print(f'atribut: {attribute}')
-        for i in range(len(data) - 1):
+        # print(f'atribut: {attribute}')
+        for i in range(len(data) - 2):
             # find two consecutive data with different target attribute (candiate for break point)
-            if data[self.target_attribute][i] != data[self.target_attribute][i + 1]:
-                break_point = (data[attribute][i] + data[attribute][i + 1]) / 2
+            if data[self.target_attribute].iloc[i] != data[self.target_attribute].iloc[i + 1]:
+                break_point = (data[attribute].iloc[i] + data[attribute].iloc[i + 1]) / 2
                 gain = self.information_gain(data, attribute, break_point)
-                print(f'Break Point: {break_point}, Gain: {gain}')
+                # print(f'Break Point: {break_point}, Gain: {gain}')
                 if gain > max_gain:
                     max_gain = gain
                     best_break_point = break_point
         return best_break_point
-
-    def set_attribute_type(self, data: dict) -> str:
-        """function for getting attribute type
-
-        Args:
-            data (dict): data
-            attribute (str): attribute
-
-        Returns:
-            str: attribute type
-        """
-        print("Ini set_attribute_type")
-        pass
-
-    def set_target_attribute_values(self, data: dict) -> list:
-        """function for getting target attribute values
-
-        Args:
-            data (dict): data
-
-        Returns:
-            list: target attribute values
-        """
-        print("Ini set_target_attribute_values")
-        pass
 
 
 
@@ -181,13 +201,7 @@ class ID3:
     """
     def print_tree(self):
         """function for printing the tree"""
-        print("Ini print_tree")
-        pass
-
-    def print_tree_recursively(self):
-        """function for printing the tree recursively"""
-        print("Ini print_tree_recursively")
-        pass
+        self.tree.print_tree()
 
 
 
@@ -212,12 +226,14 @@ class ID3:
         print("Ini save_tree")
         pass
 
+
+
 if __name__ == "__main__":
     example_data1 = {
         "Day" : ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13", "D14"],
         "Outlook" : ["Sunny", "Sunny", "Overcast", "Rain", "Rain", "Rain", "Overcast", "Sunny", "Sunny", "Rain", "Sunny", "Overcast", "Overcast", "Rain"],
-        # "Temperature" : [85, 80, 83, 70, 68, 65, 64, 72, 69, 75, 75, 72, 81, 71],
-        "Temperature" : ["Hot", "Hot", "Hot", "Mild", "Cool", "Cool", "Cool", "Mild", "Cool", "Mild", "Mild", "Mild", "Hot", "Mild"],
+        "Temperature1" : ["Hot", "Hot", "Hot", "Mild", "Cool", "Cool", "Cool", "Mild", "Cool", "Mild", "Mild", "Mild", "Hot", "Mild"],
+        "Temperature2" : [10, 11, 60, 70, 68, 12, 64, 11, 69, 75, 75, 72, 81, 11],
         "Humidity" : ["High", "High", "High", "High", "Normal", "Normal", "Normal", "High", "Normal", "Normal", "Normal", "High", "Normal", "High"],
         "Wind" : ["Weak", "Strong", "Weak", "Weak", "Weak", "Strong", "Strong", "Weak", "Weak", "Weak", "Strong", "Strong", "Weak", "Strong"],
         "PlayTennis" : ["No", "No", "Yes", "Yes", "Yes", "No", "Yes", "No", "Yes", "Yes", "Yes", "Yes", "Yes", "No"]
@@ -226,12 +242,18 @@ if __name__ == "__main__":
     example_data2 = {
         "Day" : ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13", "D14"],
         "Outlook" : ["Sunny", "Sunny", "Overcast", "Rain", "Rain", "Rain", "Overcast", "Sunny", "Sunny", "Rain", "Sunny", "Overcast", "Overcast", "Rain"],
-        # "Temperature" : [85, 80, 83, 70, 68, 65, 64, 72, 69, 75, 75, 72, 81, 71],
-        "Temperature" : ["Hot", "Hot", "Hot", "Mild", "Cool", "Cool", "Cool", "Mild", "Cool", "Mild", "Mild", "Mild", "Hot", "Mild"],
+        "Temperature1" : ["Hot", "Hot", "Hot", "Mild", "Cool", "Cool", "Cool", "Mild", "Cool", "Mild", "Mild", "Mild", "Hot", "Mild"],
+        "Temperature2" : [85, 80, 83, 70, 68, 65, 64, 72, 69, 75, 75, 72, 81, 71],
         "Humidity" : ["High", "High", "High", "High", "Normal", "Normal", "Normal", "High", "Normal", "Normal", "Normal", "High", "Normal", "High"],
         "Wind" : ["Weak", "Strong", "Weak", "Weak", "Weak", "Strong", "Strong", "Weak", "Weak", "Weak", "Strong", "Strong", "Weak", "Strong"],
         "PlayTennis" : ["Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes"]
     }
+
+    list_attribute_names1 : list = ["Outlook", "Temperature1", "Humidity", "Wind"]
+    list_attribute_names2 : list = ["Outlook", "Temperature2", "Humidity", "Wind"]
+    list_attribute_types : dict = {"Outlook": "categorical", "Temperature1": "categorical", "Temperature2": "numerical", "Humidity": "categorical", "Wind": "categorical"}
+    target_attribute : str = "PlayTennis"
+    list_target_attribute_values : list = ["Yes", "No"]
 
     df1 = pd.DataFrame(example_data1)
     df2 = pd.DataFrame(example_data2)
@@ -239,30 +261,44 @@ if __name__ == "__main__":
     # print(df2)
 
     id3 = ID3()
+    id3.list_attribute_names = list_attribute_names1
+    id3.list_attribute_types = list_attribute_types
+    id3.target_attribute = target_attribute
+    id3.list_target_attribute_values = list_target_attribute_values
 
+    print("======================= ID3 Model =======================")
+    print(id3.list_attribute_names)
+    print(id3.list_attribute_types)
+    print(id3.target_attribute)
+    print(id3.list_target_attribute_values)
+    print(id3.tree)
+    print("")
+    
     """
         Unit Test
     """
     # test plurality_value
     print("======================= Test plurality_value =======================")
     print(f'Plurality Value Outlook: {id3.plurality_value(df1, "Outlook")}')          # Sunny
-    print(f'Plurality Value Temperature: {id3.plurality_value(df1, "Temperature")}')  # Mild
+    print(f'Plurality Value Temperature: {id3.plurality_value(df1, "Temperature1")}') # Mild
     print(f'Plurality Value Humidity: {id3.plurality_value(df1, "Humidity")}')        # High
     print(f'Plurality Value Wind: {id3.plurality_value(df1, "Wind")}')                # Weak
     print(f'Plurality Value PlayTennis: {id3.plurality_value(df1, "PlayTennis")}')    # Yes
+    print("")
 
 
     # test is_homogeneous
     print("======================= Test is_homogeneous =======================")
     print(f'Is Homogeneous df1: {id3.is_homogeneous(df1)}')  # False
     print(f'Is Homogeneous df2: {id3.is_homogeneous(df2)}')  # True
+    print("")
 
 
     # test entropy
     print("======================= Test entropy =======================")
     # entropy for all data
     print(f'Entropy df1: {id3.entropy(df1)}')  # 0.940286
-    print(f'Entropy df2: {id3.entropy(df2)}')  # 0.0
+    print(f'Entropy df2: {id3.entropy(df2)}')  # 0
 
     # entropy for Outlook = Sunny
     df_sunny = df1[df1["Outlook"] == "Sunny"]
@@ -270,23 +306,28 @@ if __name__ == "__main__":
 
     # entropy for Outlook = Overcast
     df_overcast = df1[df1["Outlook"] == "Overcast"]
-    print(f'Entropy df_overcast: {id3.entropy(df_overcast)}')  # 0.0
+    print(f'Entropy df_overcast: {id3.entropy(df_overcast)}')  # 0
 
     # entropy for Outlook = Rain
     df_rain = df1[df1["Outlook"] == "Rain"]
     print(f'Entropy df_rain: {id3.entropy(df_rain)}')  # 0.970951
+    print("")
 
 
     # test information_gain
     print("======================= Test information_gain =======================")
     print(f'Information Gain Outlook: {id3.information_gain(df1, "Outlook")}')          # 0.246749
-    print(f'Information Gain Temperature: {id3.information_gain(df1, "Temperature")}')  # 0.029223
+    print(f'Information Gain Temperature: {id3.information_gain(df1, "Temperature1")}') # 0.029223
     print(f'Information Gain Humidity: {id3.information_gain(df1, "Humidity")}')        # 0.151835
     print(f'Information Gain Wind: {id3.information_gain(df1, "Wind")}')                # 0.048127
+    print("")
+
 
     # test find_best_attribute
     print("======================= Test find_best_attribute =======================")
-    print(f'Best Attribute: {id3.find_best_attribute(df1, ["Outlook", "Temperature", "Humidity", "Wind"])}')  # Outlook
+    print(f'Best Attribute: {id3.find_best_attribute(df1, ["Outlook", "Temperature1", "Humidity", "Wind"])}')  # Outlook
+    print("")
+
 
     # test break_point
     print("======================= Test break_point =======================")
@@ -296,7 +337,18 @@ if __name__ == "__main__":
     }
     df3 = pd.DataFrame(data3)
     print(f'Best Break Point: {id3.break_point(df3, "Temperature2")}')
+    print("")
     # Best Break Point: 54.0, Gain: 0.459148
     # Break Point: 54.0, Gain: 0.459148
     # Break Point: 85.0, Gain: 0.190875
 
+
+    # test train
+    print("======================= Test train =======================")
+    id3.tree = id3.train(df1, id3.list_attribute_names, {})
+    id3.print_tree()
+    print("")
+    id3.list_attribute_names = list_attribute_names2
+    id3.tree = id3.train(df1, id3.list_attribute_names, {})
+    id3.print_tree()
+    print("")
